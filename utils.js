ff49279ae3409a7e983eeed830fd833083100394
yuval <nhajeho1@gmail.com>
2022-12-17 21:43:10 +0200

    made generateRandomCreature more robust

--- utils.js
+++ utils.js
@@ -6,38 +6,27 @@ import { Vector2 } from "./Vector.js";
 /**
  * Generates a random creature with the given options.
  *
- * @param {Object} [options] - The options for creating the creature.
- * @param {Vector2} [options.position=new Vector2(0, 0)] - The initial position of the creature.
- * @param {Vector2} [options.velocity=new Vector2(0, 0)] - The initial velocity of the creature.
- * @param {number} [options.mass=1] - The mass of the creature.
- * @param {Object} [options.joints] - An object with two properties: coords and connections.
- * @param {Vector2[]} [options.joints.coords=[]] - The coordinates of the joint.
- * @param {import("./Creature.js").JointConnection[]} [options.joints.connections=[]] - The connections between the joints.
- * @param {number} [minJoints=3] - The minimum number of joints to generate.
- * @param {number} [maxJoints=12] - The maximum number of joints to generate.
- * @param {number} [minTension=0] - The minimum tension value to generate.
- * @param {number} [maxTension=10] - The maximum tension value to generate.
- * @param {number} [minSteps=1] - The minimum number of steps to generate for each connection's animation.
- * @param {number} [maxSteps=5] - The maximum number of steps to generate for each connection's animation.
- * @param {number} [minOffset=-10] - The minimum offset value to generate for each step in the connection's animation.
- * @param {number} [maxOffset=10] - The maximum offset value to generate for each step in the connection's animation.
- * @param {number} [minDelay=100] - The minimum delay (in milliseconds) to generate for each step in the connection's animation.
- * @param {number} [maxDelay=1000] - The maximum delay (in milliseconds) to generate for each step in the connection's animation.
+ * @param {Object} [creatureOptions] - The options for the creature.
+ * @param {Vector2} [creatureOptions.position=new Vector2(0, 0)] - The initial position of the creature.
+ * @param {Vector2} [creatureOptions.velocity=new Vector2(0, 0)] - The initial velocity of the creature.
+ * @param {number} [creatureOptions.mass=1] - The mass of the creature.
+ * @param {Object} [creatureOptions.joints] - An object with two properties: coords and connections.
+ * @param {Vector2[]} [creatureOptions.joints.coords=[]] - The coordinates of the joint.
+ * @param {import("./Creature.js").JointConnection[]} [creatureOptions.joints.connections=[]] - The connections between the joints.
+ * @param {Object} [generationOptions] - The options for the generation process.
+ * @param {number} [generationOptions.minJoints=3] - The minimum number of joints to generate.
+ * @param {number} [generationOptions.maxJoints=12] - The maximum number of joints to generate.
+ * @param {number} [generationOptions.minTension=0] - The minimum tension value to generate.
+ * @param {number} [generationOptions.maxTension=10] - The maximum tension value to generate.
+ * @param {number} [generationOptions.minSteps=1] - The minimum number of steps to generate for each connection's animation.
+ * @param {number} [generationOptions.maxSteps=5] - The maximum number of steps to generate for each connection's animation.
+ * @param {number} [generationOptions.minOffset=-10] - The minimum offset value to generate for each step in the connection's animation.
+ * @param {number} [generationOptions.maxOffset=10] - The maximum offset value to generate for each step in the connection's animation.
+ * @param {number} [generationOptions.minDelay=100] - The minimum delay (in milliseconds) to generate for each step in the connection's animation.
+ * @param {number} [generationOptions.maxDelay=1000] - The maximum delay (in milliseconds) to generate for each step in the connection's animation.
  * @return {Creature} A new creature with random joints and connections.
  */
-export function generateRandomCreature(
-  options,
-  minJoints = 3,
-  maxJoints = 12,
-  minTension = 0,
-  maxTension = 10,
-  minSteps = 1,
-  maxSteps = 5,
-  minOffset = -10,
-  maxOffset = 10,
-  minDelay = 100,
-  maxDelay = 1000
-) {
+export function generateRandomCreature(creatureOptions, generationOptions) {
   /**
    * Set default options
    * @typedef {Object} UsedOptions - The options for creating the creature.
@@ -48,6 +37,26 @@ export function generateRandomCreature(
    * @property {Vector2[]} joints.coords - The coordinates of the joint.
    * @property {import("./Creature.js").JointConnection[]} joints.connections - The connections between the joints.
    */
+
+  let {
+    minJoints = 3,
+    maxJoints = 12,
+    minTension = 0,
+    maxTension = 10,
+    minSteps = 1,
+    maxSteps = 5,
+    minOffset = -10,
+    maxOffset = 10,
+    minDelay = 100,
+    maxDelay = 1000,
+  } = generationOptions || {};
+
+  [minJoints, maxJoints] = updateMinMax(minJoints, maxJoints);
+  [minTension, maxTension] = updateMinMax(minTension, maxTension);
+  [minSteps, maxSteps] = updateMinMax(minSteps, maxSteps);
+  [minOffset, maxOffset] = updateMinMax(minOffset, maxOffset);
+  console.log(minOffset, maxOffset);
+  [minDelay, maxDelay] = updateMinMax(minDelay, maxDelay);
 
   /** @type {UsedOptions} */
   const usedOptions = {
@@ -60,7 +69,7 @@ export function generateRandomCreature(
       // @ts-ignore
       connections: [],
     },
-    ...options,
+    ...creatureOptions,
   };
 
   // Generate random number of joints within the specified range
@@ -106,6 +115,7 @@ export function generateRandomCreature(
       connection.animation?.push(animation);
     }
   }
+  console.log(usedOptions);
   return new Creature(usedOptions);
 }
 
@@ -121,4 +131,14 @@ export function getRandomNum(max, min, int = false) {
   }
   const num = Math.random() * (max - min + 1) + min;
   return int ? Math.floor(num) : num;
+}
+
+/**
+ * @param {number} min
+ * @param {number} max
+ * @returns {[number, number]}
+ */
+function updateMinMax(min, max) {
+  if (min > max) max = min;
+  return [min, max];
 }
